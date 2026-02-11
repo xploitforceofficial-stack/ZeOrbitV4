93486cce5af17794a21229fe97be02414b12425f
xploitforceofficial-stack <xploitforceofficial@gmail.com>
2026-02-11 14:14:44 +0700

    Update ZeOrbit.js

--- api/loader/ZeOrbit.js
+++ api/loader/ZeOrbit.js
@@ -1,22 +1,20 @@
 export default function handler(req, res) {
     const userAgent = req.headers['user-agent'] || '';
 
-    // Cek apakah diakses lewat browser
     if (userAgent.includes('Mozilla') || userAgent.includes('Chrome')) {
+        // Tampilan 404 palsu untuk Browser
         res.status(404).send(`
-            <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
             <html>
                 <head><title>404 Not Found</title></head>
-                <body style="font-family: Arial; text-align: center; margin-top: 50px;">
-                    <h1>404 Not Found</h1>
+                <body style="font-family: Arial, sans-serif; text-align: center; margin-top: 50px;">
+                    <h1>Not Found</h1>
                     <p>The requested URL was not found on this server.</p>
-                    <hr>
-                    <address>Apache/2.4.41 (Ubuntu) Server at ze-orbit-v4.vercel.app Port 443</address>
+                    <hr><address>Apache/2.4.41 (Ubuntu) Server at ze-orbit-v4.vercel.app Port 443</address>
                 </body>
             </html>
         `);
     } else {
-        // Jika diakses executor, arahkan ke raw github
+        // Redirect ke script asli untuk Executor
         res.redirect('https://raw.githubusercontent.com/xploitforceofficial-stack/zeorbitv42/refs/heads/main/obfuscated_script-1770792805723.lua');
     }
 }
